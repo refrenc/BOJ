608730d793d342d09e49c9451d657d89ae379ea6
Joo Sang Ah <52567584+refrenc@users.noreply.github.com>
2024-01-21 21:55:53 +0900

    [Silver II] Title: 최대 힙, Time: 120 ms, Memory: 37044 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+heap = []
+
+for _ in range(n):
+    input = int(sys.stdin.readline().rstrip())
+    if input == 0:
+        if len(heap) < 1:
+            sys.stdout.write("0")
+        else:
+            sys.stdout.write(str(-heapq.heappop(heap)))
+        sys.stdout.write("\n")
+        continue
+    heapq.heappush(heap, -input)
